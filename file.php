525c5e2a9a3d4b3ffe309e009bbbcf59ef1c8ba4
sjd89s89dd <45253797+sjd89s89dd@users.noreply.github.com>
2018-12-12 09:04:06 -0800

    Update file.php

--- file.php
+++ file.php
@@ -18,7 +18,9 @@ while(1)
 	$http .= "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0\r\n";
 	//$http .= "Content-Type: application/x-www-form-urlencoded\r\n";
 	//$http .= "Content-length: " . strlen($post_data) . "\r\n";
-	$http .= "Connection: close\r\n\r\n";
+	$http .= "Connection: close\r\n";
+	$cookie = "__cfduid=d4f9a7ad1fbbda06f3775688e9ca479181544621882; PHPSESSID=gr9mbin6kqiofui6dt3oenbel4; TawkConnectionTime=0; __tawkuuid=e::accounts.4xincome.com::jDDWAhG0hEeUN3RPXRvDGbdQFUiZiASR331OUtbgkTcMvcATVYIZD/x5Yg5OrRTU::2; cf_clearance=20c6ac3db307187db5b7a90a46269bbd38758654-1544633476-1800-150";
+	$http .= "Cookie: " .$cookie. "\r\n";
 	//$http .= $post_data . "\r\n";
 	
 	fwrite($socket, $http);
